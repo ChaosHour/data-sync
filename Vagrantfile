fcf12cce5f38f64a771a6ec6475080dbcbf5324b
ChaosHour <kurt_lv@cox.net>
2023-04-23 17:09:41 -0700

    adding files after removing some generic passwords

---
+++ Vagrantfile
@@ -0,0 +1,109 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# User Variables
+BOX_OS_BASE = "ubuntu/jammy64" 
+BOX_NAME = "Ubuntu LTS"
+BOX_CPU_COUNT = "2"
+BOX_RAM_MB = "1024"
+
+
+
+
+Vagrant.configure("2") do |config|
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://vagrantcloud.com/search.
+  
+  # Ubuntu 22.04 LTS "Jammy Jellyfish" option for base image
+  config.vm.box = BOX_OS_BASE
+  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"    
+  
+  config.vm.define "proxysql" do |proxysql|
+    proxysql.vm.hostname = "proxysql"
+    #proxysql.vm.network "forwarded_port", guest: 6032, host: 6336
+    #proxysql.vm.network "forwarded_port", guest: 6031, host: 6331
+    proxysql.vm.network :public_network, :ip => "10.8.0.150", :bridge => "en6: AX88179A", :use_dhcp_assigned_default_route => true
+  end
+  
+  sleep 10
+
+  config.vm.define "orchestrator" do |orchestrator|
+    orchestrator.vm.hostname = "orchestrator"
+    #orchestrator.vm.network "forwarded_port", guest: 3000, host: 3330
+    orchestrator.vm.network :public_network, :ip => "10.8.0.151", :bridge => "en6: AX88179A", :use_dhcp_assigned_default_route => true
+  end
+
+  sleep 10
+
+  config.vm.define "primary" do |primary|
+    primary.vm.hostname = "primary"
+    #primary.vm.network "forwarded_port", guest: 3306, host: 3337
+    primary.vm.network :public_network, :ip => "10.8.0.152", :bridge => "en6: AX88179A", :use_dhcp_assigned_default_route => true
+  end
+
+ config.vm.define "replica" do |replica|
+     replica.vm.hostname = "replica"
+     #replica.vm.network "forwarded_port", guest: 3306, host: 3338
+     replica.vm.network :public_network, :ip => "10.8.0.153", :bridge => "en6: AX88179A", :use_dhcp_assigned_default_route => true
+   end
+
+ config.vm.define "etlreplica" do |etlreplica|
+    etlreplica.vm.hostname = "etlreplica"
+    #etlreplica.vm.network "forwarded_port", guest: 3306, host: 3339
+    etlreplica.vm.network :public_network, :ip => "10.8.0.154", :bridge => "en6: AX88179A", :use_dhcp_assigned_default_route => true
+  end
+
+  config.vm.provider "virtualbox" do |vb|      
+    #vb.name = BOX_NAME
+    vb.memory = BOX_RAM_MB
+    vb.cpus = BOX_CPU_COUNT
+  end  
+  
+  sleep 20
+  
+  config.vm.provision "shell", inline: <<-SHELL
+   apt-get update
+   apt-get -y dist-upgrade
+   add-apt-repository ppa:longsleep/golang-backports
+   apt-get install -y bash build-essential module-assistant virtualbox-ose-guest-utils virtualbox-ext-pack
+   apt-get install -y cifs-utils curl openssh-server openssh-client 
+   apt-get install -y screen tree   
+   apt-get install -y perl python3 python3-dev python3-pip telnet vim-common gnupg software-properties-common
+   apt-get install  -y libpq-dev
+   apt-get install -y golang-1.20
+   apt -y install golang-go
+   apt-get install -y libxt6 libxmu6
+   apt-get install -y bpfcc-tools linux-headers-$(uname -r) 
+   SHELL
+   
+   
+   if File.exist?("secrets.yml")
+    require 'yaml'
+    uPass = YAML::load(File.open('secrets.yml'))
+  #Additional provisioning with shell script to create customized user
+  config.vm.provision "shell", inline:  <<-SHELL2
+    sudo su - root
+    useradd -m -s /bin/bash -U #{uPass['user']}
+    usermod -aG sudo #{uPass['user']}
+    echo '#{uPass['user']}:#{uPass['pass']}' | chpasswd
+    cp -pr /home/vagrant/.ssh /home/#{uPass['user']}/
+    chown -R #{uPass['user']}:#{uPass['user']} /home/#{uPass['user']}
+    echo '3' | update-alternatives --config editor    
+    wait
+    shutdown -r now
+    SHELL2
+
+    sleep 20
+
+#    config.vm.provision "ansible" do |ansible|
+#      ansible.verbose = "v"
+#      ansible.playbook = "build.yml"
+#      ansible.become = true
+#      #ansible.compatibility_mode = "2.0"
+#      ansible.raw_arguments = "-v"
+#      ansible.become = true
+#      #ansible.raw_arguments  = "--vault-password-file=vars/vault_pass.txt"
+#    end    
+
+  end
+end
---
+++ roles/proxysql/files/rules.sql
@@ -0,0 +1,88 @@
+-- # Read write splitting
+-- #INSERT INTO mysql_servers(hostgroup_id, hostname, port) VALUES (0,'192.168.50.2',3306);
+-- #INSERT INTO mysql_servers(hostgroup_id, hostname, port) VALUES (1,'192.168.50.3',3306);
+-- #INSERT INTO mysql_servers(hostgroup_id, hostname, port) VALUES (1,'192.168.50.4',3306);
+-- #INSERT INTO mysql_users (username,password) VALUES ('chaos','{{ chaos_pass }}');
+-- #INSERT INTO mysql_query_rules(active,match_pattern,destination_hostgroup,apply) VALUES(1,'^SELECT.*FOR UPDATE$',0,1);
+-- #INSERT INTO mysql_query_rules(active,match_pattern,destination_hostgroup,apply) VALUES(1,'^SELECT',1,1);
+
+-- #LOAD MYSQL SERVERS TO RUNTIME;
+-- #LOAD MYSQL USERS TO RUNTIME;
+-- #LOAD MYSQL QUERY RULES TO RUNTIME;
+
+-- #SAVE MYSQL SERVERS TO DISK;
+-- #SAVE MYSQL USERS TO DISK;
+-- #SAVE MYSQL QUERY RULES TO DISK;
+
+
+
+-- # Failover:-
+-- INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '192.168.50.2', 3306, 20);
+-- INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '192.168.50.3', 3306, 20);
+-- INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '192.168.50.4', 3306, 20);
+-- INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '192.168.50.2', 3306, 20);
+-- INSERT INTO mysql_users (username,password) VALUES ('chaos','{{ chaos_pass }}');
+-- #INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('chaos', '{{ chaos_pass }}', 1, 0, 2000);
+
+-- INSERT INTO mysql_replication_hostgroups (writer_hostgroup, reader_hostgroup) VALUES (0, 1);
+-- INSERT INTO mysql_query_rules (active, match_pattern, destination_hostgroup, cache_ttl, apply) VALUES (1, '^SELECT .* FOR UPDATE', 0, NULL, 1);
+-- INSERT INTO mysql_query_rules (active, match_pattern, destination_hostgroup, cache_ttl, apply) VALUES (1, '^SELECT .*', 1, NULL, 1);
+
+
+-- #UPDATE global_variables SET variable_value="monuser" WHERE variable_name="mysql-monitor_username";
+-- #UPDATE global_variables SET variable_value="monpass" WHERE variable_name="mysql-monitor_password";
+-- SET mysql-monitor_username='monuser';
+-- SET mysql-monitor_password='{{ monuser_pass }}';
+-- SET mysql-connect_timeout_server_max=20000;
+
+-- LOAD MYSQL SERVERS TO RUNTIME;
+-- LOAD MYSQL USERS TO RUNTIME;
+-- LOAD MYSQL VARIABLES TO RUNTIME;
+-- LOAD MYSQL QUERY RULES TO RUNTIME;
+
+-- SAVE MYSQL SERVERS TO DISK;
+-- SAVE MYSQL USERS TO DISK;
+-- SAVE MYSQL VARIABLES TO DISK;
+-- SAVE MYSQL QUERY RULES TO DISK;
+
+-- # Failover:
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '10.8.0.152', 3306, 20);
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '10.8.0.153', 3306, 20);
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '10.8.0.154', 3306, 20);
+INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (1, '10.8.0.152', 3306, 20);
+
+
+INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('chaos', '{{ chaos_pass }}', 1, 0, 2000);
+INSERT INTO mysql_replication_hostgroups (writer_hostgroup,reader_hostgroup,comment) VALUES (1,2,'cluster1');
+-- INSERT INTO mysql_users (username,password) VALUES ('chaos','{{ chaos_pass }}');
+INSERT INTO mysql_users(username,password,default_hostgroup) VALUES ('root','{{ mysql_root_pass }}',1);
+INSERT INTO mysql_users(username,password,default_hostgroup) VALUES ('dba_util','{{ dba_util_pass }}',1);
+INSERT INTO mysql_replication_hostgroups (writer_hostgroup, reader_hostgroup) VALUES (0, 1);
+INSERT INTO mysql_query_rules (active, match_pattern, destination_hostgroup, cache_ttl, apply) VALUES (1, '^SELECT .* FOR UPDATE', 0, NULL, 1);
+INSERT INTO mysql_query_rules (active, match_pattern, destination_hostgroup, cache_ttl, apply) VALUES (1, '^SELECT .*', 1, NULL, 1);
+
+UPDATE global_variables SET variable_value='monuser' WHERE variable_name='mysql-monitor_username';
+UPDATE global_variables SET variable_value='monpass' WHERE variable_name='mysql-monitor_password';
+UPDATE global_variables SET variable_value='2000' WHERE variable_name IN ('mysql-monitor_connect_interval','mysql-monitor_ping_interval','mysql-monitor_read_only_interval');
+
+SET mysql-monitor_username='monuser';
+SET mysql-monitor_password='{{ monuser_pass }}';
+SET mysql-connect_timeout_server_max=20000;
+
+LOAD MYSQL VARIABLES TO RUNTIME;
+SAVE MYSQL VARIABLES TO DISK;
+
+LOAD MYSQL SERVERS TO RUNTIME;
+SAVE MYSQL SERVERS TO DISK;
+SAVE MYSQL VARIABLES TO DISK;
+
+
+LOAD MYSQL SERVERS TO RUNTIME;
+LOAD MYSQL USERS TO RUNTIME;
+LOAD MYSQL VARIABLES TO RUNTIME;
+LOAD MYSQL QUERY RULES TO RUNTIME;
+
+SAVE MYSQL SERVERS TO DISK;
+SAVE MYSQL USERS TO DISK;
+SAVE MYSQL VARIABLES TO DISK;
+SAVE MYSQL QUERY RULES TO DISK;
